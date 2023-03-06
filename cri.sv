600a71deaf5f4b9ae01700d61e4b2560e6179a80
Prithvi Bhat <prithvinarayanbhat@gmail.com>
2023-03-06 17:29:07 -0600

    Fixed: Addressing in Ram module

--- cri.sv
+++ cri.sv
@@ -30,9 +30,9 @@ module cri(
 
     always_ff @ (posedge KEY[1])                // Trigger on posedge of Key press
     begin
-        if (counter > 9)     counter = -1;      // Reset counter
+        if (counter++ >= 5)     counter = -1;      // Reset counter
 
-        counter++;                              // Increment counter value
+        // counter++;                              // Increment counter value
 
         case (counter)
             32'd0:
@@ -42,34 +42,18 @@ module cri(
                 width       = 2'b0;             // Size: Byte
                 alu_out     = 32'h50;           // Address
                 rs2_data    = 32'h80;           // Test data
-
             end
 
             32'd1:
             begin
-                d_we        = 1'b0;             // Write disabled
-                width       = 2'b0;             // Size: Byte
-                alu_out     = 32'h50;           // Address
-            end
-
-            32'd2:
-            begin
                 d_we        = 1'b1;             // Write enabled
                 sign        = 1'b0;             // Unsigned data
                 width       = 2'b0;             // Size: Byte
-                alu_out     = 32'h47;           // Address
+                alu_out     = 32'h53;           // Address
                 rs2_data    = 32'h50;           // Test data
-
             end
 
-            32'd3:
-            begin
-                d_we        = 1'b0;             // Write disabled
-                width       = 2'b10;            // Size: Word
-                alu_out     = 32'h50;           // Address
-            end
-
-            32'd4:
+            32'd2:
             begin
                 d_we        = 1'b1;             // Write enabled
                 sign        = 1'b0;             // Unsigned data
@@ -78,30 +62,16 @@ module cri(
                 rs2_data    = 32'h1191;         // Test data
             end
 
-            32'd5:
-            begin
-                d_we        = 1'b0;             // Write disabled
-                width       = 2'b00;            // Size: Half Word
-                alu_out     = 32'b10;           // Address
-            end
-
-            32'd6:
+            32'd3:
             begin
                 d_we        = 1'b1;             // Write enabled
                 sign        = 1'b0;             // Unsigned data
                 width       = 2'b10;            // Size: Word
-                alu_out     = 32'h56;           // Address
+                alu_out     = 32'h60;           // Address
                 rs2_data    = 32'h12345678;     // Test data
             end
 
-            32'd7:
-            begin
-                d_we        = 1'b0;             // Write disabled
-                width       = 2'b10;            // Size: Word
-                alu_out     = 32'h56;           // Address
-            end
-
-            32'd8:
+            32'd4:
             begin
                 d_we        = 1'b1;             // Write enabled
                 sign        = 1'b1;             // Unsigned data
@@ -110,20 +80,14 @@ module cri(
                 rs2_data    = 32'hFFFB;         // Test data
             end
 
-            32'd9:
-            begin
-                d_we        = 1'b0;             // Write disabled
-                width       = 2'b01;            // Size: Half Word
-                alu_out     = 32'h60;           // Address
-            end
         endcase
     end
 
     // To test dual port RAM module
     rv32i_syncDualPortRam ramTest(
         .clk(ADC_CLK_10),
-        .i_addr(alu_out),
-        .d_addr(alu_out),
+        .i_addr(alu_out[31:2]),
+        .d_addr(alu_out[31:2]),
         .d_we(d_we),
         .d_be(writeBankEnable(alu_out[1:0], width)),
         .d_wdata(shifted_rs2_data(rs2_data, alu_out[1:0])),
--- rv32i_memInterfaceTest.sv
+++ rv32i_memInterfaceTest.sv
@@ -1,11 +1,11 @@
-module blob(
+module rv32i_memInterfaceTest(
         input clk,
         input [1:0] KEY,
         output [31:0] func_return
     );
 
     `include "rv32i_memInterface.sv"            // Include files
-error
+
     wire [31:0] rs2_data, d_rdata, alu_out;     // 32 bit values
     wire [1:0] width, bank;                     // 02 bit values
     wire sign, w_enable, d_we;                  // 01 bit values
@@ -14,7 +14,7 @@ error
 
     always_ff @ (posedge KEY[1])                // Trigger on posedge of Key press
     begin
-        if (counter > 7)     counter = -1;      // Reset counter
+        if (counter > 9)     counter = -1;      // Reset counter
 
         counter++;                              // Increment counter value
 
@@ -23,85 +23,58 @@ error
             begin
                 d_we        = 1'b1;             // Write enabled
                 sign        = 1'b0;             // Unsigned data
-                width       = 1'b0;             // Size: Byte
+                width       = 2'b0;             // Size: Byte
                 alu_out     = 32'h50;           // Address
                 rs2_data    = 32'h80;           // Test data
-
             end
 
             32'd1:
             begin
-                d_we        = 1'b0;             // Write disabled
+                d_we        = 1'b1;             // Write enabled
                 sign        = 1'b0;             // Unsigned data
-                width       = 2'd1;             // Size: Byte
-                alu_out     = 32'h50;           // Address
-                rs2_data    = 32'h100;          // Test data
+                width       = 2'b0;             // Size: Byte
+                alu_out     = 32'h53;           // Address
+                rs2_data    = 32'h50;           // Test data
             end
 
             32'd2:
             begin
                 d_we        = 1'b1;             // Write enabled
                 sign        = 1'b0;             // Unsigned data
-                width       = 2'd0;             // Size: Byte
-                alu_out     = 32'h51;           // Address
+                width       = 2'b0;             // Size: Byte
+                alu_out     = 32'b10;           // Address
                 rs2_data    = 32'h1191;         // Test data
             end
 
             32'd3:
             begin
-                d_we        = 1'b0;             // Write disabled
+                d_we        = 1'b1;             // Write enabled
                 sign        = 1'b0;             // Unsigned data
-                width       = 2'd1;             // Size: Half Word
-                alu_out     = 32'h51;           // Address
-                rs2_data    = 32'h200;          // Test data
+                width       = 2'b10;            // Size: Word
+                alu_out     = 32'h56;           // Address
+                rs2_data    = 32'h12345678;     // Test data
             end
 
             32'd4:
             begin
                 d_we        = 1'b1;             // Write enabled
-                sign        = 1'b0;             // Unsigned data
-                width       = 2'd2;             // Size: Word
-                alu_out     = 32'hC;            // Address
-                rs2_data    = 32'h12345678;     // Test data
+                sign        = 1'b1;             // Unsigned data
+                width       = 2'b01;            // Size: Half Word
+                alu_out     = 32'h60;           // Address
+                rs2_data    = 32'hFFFB;         // Test data
             end
 
-            32'd5:
-            begin
-                d_we        = 1'b0;             // Write disabled
-                sign        = 1'b0;             // Unsigned data
-                width       = 2'd2;             // Size: Word
-                alu_out     = 32'hC;            // Address
-                rs2_data    = 32'h300;          // Test data
-            end
-
-            32'd6:
-            begin
-                d_we        = 1'b1;             // Write enabled
-                sign        = 1'b0;             // Unsigned data
-                width       = 2'd1;             // Size: Half Word
-                alu_out     = 32'h52;           // Address
-                rs2_data    = -5;               // Test data
-            end
-
-            32'd7:
-            begin
-                d_we        = 1'b0;             // Write disabled
-                sign        = 1'b1;             // Signed data
-                width       = 2'd2;             // Size: Half Word
-                alu_out     = 32'h52;           // Address
-                rs2_data    = 32'h400;          // Test data
-            end
         endcase
     end
 
     // To test dual port RAM module
     rv32i_syncDualPortRam ramTest(
         .clk(ADC_CLK_10),
-        .i_addr(alu_out),
-        .d_addr(alu_out),
+        .i_addr(alu_out[31:2]),
+        .d_addr(alu_out[31:2]),
         .d_we(d_we),
-        .d_be(writeBankEnable(alu_out, width)),
-        .d_wdata(shifted_rs2_data(rs2_data, alu_out)),
+        .d_be(writeBankEnable(alu_out[1:0], width)),
+        .d_wdata(shifted_rs2_data(rs2_data, alu_out[1:0])),
         .d_rdata(d_rdata)
     );
 
