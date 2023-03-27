35db4f94bb48a6b351934cc5d718766741950165
Prithvi Bhat <prithvinarayanbhat@gmail.com>
2023-03-26 20:26:24 -0500

    Added support for Ebreak and aligned IW and PC from ID

--- cri.sv
+++ cri.sv
@@ -1,17 +1,5 @@
 /*
     Top code to test all other modules
-
-module cri(
-        input ADC_CLK_10,
-        input [1:0] KEY,
-        input [9:0] SW
-    );
-
-    // rv32i_memTest memTest (.clk(ADC_CLK_10), .KEY(KEY), .SW(SW));         // To test memory module
-    // rv32i_exTest exTest (.clk(ADC_CLK_10), .KEY(KEY), .SW(SW));           // To test ALU module
-    rv32i_memInterfaceTest memInterfaceTest (.clk(ADC_CLK_10), .KEY(KEY));  // To test memory interface module
-
-endmodule
 */
 module cri(
         input ADC_CLK_10,
