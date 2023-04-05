ddc0ebd7016d869f15b1c9ed1afb636607487347
Prithvi Bhat <prithvinarayanbhat@gmail.com>
2023-04-05 13:56:58 -0500

    Initial work in handling Branch hazards

--- rv32i_ifTop.sv
+++ rv32i_ifTop.sv
@@ -1,33 +1,33 @@
 module rv32i_ifTop(
         input clk, reset,               // System Clock and Snchronous Reset
         input [31:0] memIfData,         // From MemInterface
+
+        input jump_enable,              // Enable jump          | From idTop module
+        input [31:0] jump_addr,         // Address to jump to   | From idTop module
+
         output reg [31:2] memIfAddr,    // To MemInterface
         output reg [31:0] iw_out,       // To ID
         output reg [31:0] pc_out        // To ID
     );
 
     reg [31:0] pc = 32'd0;
-    reg [31:00] pc_int;
+    reg [31:00] pc_int = 0;
 
     // set program counter
-    always_ff @ (posedge(clk))
+    always_ff @ (posedge clk)
     begin
-        if (reset) pc = 32'd0;      // Reset Program Counter
+        if (reset) pc <= 32'd0;                     // Reset Program Counter
         else
         begin
-            pc <= pc + 32'd4;       // Increment Program Counter
-            memIfAddr <= pc[31:02]; // PC drives memIfAddr directly
-            pc_int = pc;
-
+            if (jump_enable)    pc <= jump_addr;    // Set Jump destination
+            else                pc <= pc + 32'd4;   // Increment Program Counter
         end
-
-    end
-
-    always_ff @ (posedge(clk))
-    begin
+        pc_int = pc;
         pc_out <= pc_int;
     end
 
-    assign iw_out = memIfData;      // Instruction Word (memIfData) is registered in the memory module, so it directly drives iw_out
+    assign iw_out = memIfData;                      // Instruction Word (memIfData) is registered in the memory module, so it directly drives iw_out
+    assign memIfAddr = pc[31:02];                   // PC drives memIfAddr directly
+
 
 endmodule
--- script.sh
+++ script.sh
@@ -1,25 +1,20 @@
 #!/bin/bash
 
-echo "Compiling now.."
-
-if [ $# -eq 1 ];
+if [ $# -le 1 ];
 then
-    echo "compiling now.."
-    riscv32-unknown-elf-gcc -o $1 $1.s -ffreestanding -nostdlib -march=rv32i -mabi=ilp32
+    riscv32-unknown-elf-gcc -o cri_asm cri_asm.s -ffreestanding -nostdlib -march=rv32i -mabi=ilp32
 
-    riscv32-unknown-elf-objdump -dr $1 > $1.asm_dump
 
-    riscv32-unknown-elf-objcopy $1 -O binary $1.bin
+    riscv32-unknown-elf-objcopy cri_asm -O binary cri_asm.bin
 
-    hexdump $1.bin
 
-    riscv32-unknown-elf-gcc -o $1 $1.s -march=rv32i -mabi=ilp32 -nostdlib -T/home/prithvi/Documents/Academics/Semester-II/CSE-6351/Labs/Lab1/Part-1/cse4372_riscv.ld
+    riscv32-unknown-elf-gcc -o cri_asm cri_asm.s -march=rv32i -mabi=ilp32 -nostdlib -T/home/prithvi/Documents/Academics/Semester-II/CSE-6351/Labs/Lab1/Part-1/cse4372_riscv.ld
 
-    objcopy -O ihex $1 $1.hex
+    objcopy -O ihex cri_asm cri_asm.hex
 
-    /home/prithvi/Documents/Academics/Semester-II/CSE-6351/Labs/Lab1/Part-1/hex_intel_to_quartis $1.hex ram.hex
+    /home/prithvi/Documents/Academics/Semester-II/CSE-6351/Labs/Lab1/Part-1/hex_intel_to_quartis cri_asm.hex ram.hex
 
-    riscv32-unknown-elf-objdump -dr $1
+    riscv32-unknown-elf-objdump -dr cri_asm
 else
     echo "Insufficient args"
 fi
